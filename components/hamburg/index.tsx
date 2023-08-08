b4cb389423e5bd25382c64f41fa2d29f87d01c14
bruno <bruno_am_22@hotmail.com>
2023-08-08 18:07:14 -0300

    dcdc

--- components/hamburg/index.tsx
+++ components/hamburg/index.tsx
@@ -1,10 +1,19 @@
-import { DivHamburg } from "./styled";
+import { DivHamburg,DivDev ,DivEnlaces} from "./styled";
+import { SubTitle } from "@/ui/typography";
+import Linkedin from "@/ui/icons/linkedin.svg"
+import Github from '@/ui/icons/github.svg'
+
 export function Hamburg() {
    return (
       <DivHamburg>
-      <div>Primeror</div>
-         <div>Primeror</div>
-         <div>Primeror</div>
+         <DivEnlaces>
+            <a href=""><SubTitle>Mapa</SubTitle></a>
+            <a href=""><SubTitle>Sobre nosotros</SubTitle></a>
+         </DivEnlaces>
+         <DivDev>
+            <Linkedin/>   
+            <Github/>
+         </DivDev>   
       </DivHamburg>
    );
 }
