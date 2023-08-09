08be6e7b98707d2b09330eedeff423f5ba3b8c1b
brunoken22 <bruno_am_22@hotmail.com>
2023-08-09 19:51:51 -0300

    funcion

--- app/mapa/page.tsx
+++ app/mapa/page.tsx
@@ -1,25 +1,44 @@
 'use client';
 import mapboxgl from 'mapbox-gl';
-mapboxgl.accessToken =
-  'pk.eyJ1IjoiZGVuaXNwYXJhZGEiLCJhIjoiY2t2cmhwbjZlMDM5czJ2cWlyczZoODg4cSJ9.6obRc3i_TK7qdx_A6_y-qg';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import {useEffect, useRef, useState} from 'react';
-import {DivMap, Sidebar} from '@/ui/contenedores';
+import {DivMap, Sidebar, DivSearch} from '@/ui/contenedores';
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
+
+mapboxgl.accessToken =
+  'pk.eyJ1IjoiZGVuaXNwYXJhZGEiLCJhIjoiY2t2cmhwbjZlMDM5czJ2cWlyczZoODg4cSJ9.6obRc3i_TK7qdx_A6_y-qg';
 export default function Mapa() {
   const mapContainer = useRef(null);
   const map: any = useRef(null);
-  const [lng, setLng] = useState(-70.9);
-  const [lat, setLat] = useState(42.35);
+  const geocoderContainer: any = useRef(null); // Ref para el contenedor del geocoder
+  const [lng, setLng] = useState(-58.6645);
+  const [lat, setLat] = useState(-34.6747);
   const [zoom, setZoom] = useState(9);
+
   useEffect(() => {
-    if (map?.current) return; // initialize map only once
+    if (map.current) return; // initialize map only once
     map.current = new mapboxgl.Map({
       container: mapContainer.current as any,
       style: 'mapbox://styles/mapbox/streets-v12',
       center: [lng, lat],
       zoom: zoom,
     });
-    map.current?.on('move', () => {
+    const geocoder = new MapboxGeocoder({
+      accessToken: mapboxgl.accessToken,
+      mapboxgl: mapboxgl,
+    });
+
+    // Agregar el geocoder al contenedor
+    geocoderContainer.current.appendChild(geocoder.onAdd(map.current));
+
+    // map.current.addControl(
+    //   new MapboxGeocoder({
+    //     accessToken: mapboxgl.accessToken,
+    //     mapboxgl: mapboxgl,
+    //   })
+    // );
+
+    map.current.on('move', () => {
       setLng(map.current.getCenter().lng.toFixed(4));
       setLat(map.current.getCenter().lat.toFixed(4));
       setZoom(map.current.getZoom().toFixed(2));
@@ -28,14 +47,15 @@ export default function Mapa() {
 
   return (
     <DivMap>
+      <link
+        rel='stylesheet'
+        href='https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v5.0.0/mapbox-gl-geocoder.css'
+        type='text/css'></link>
+      <DivSearch ref={geocoderContainer}></DivSearch>
       <Sidebar>
         Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
       </Sidebar>
-      <div
-        ref={mapContainer}
-        className='map-container'
-        style={{height: '100%'}}
-      />
+      <div ref={mapContainer} className='map' style={{height: '100%'}} />
     </DivMap>
   );
 }
--- next.config.js
+++ next.config.js
@@ -23,6 +23,7 @@ const nextConfig = withSvgr({
          },
       ];
    },
+
 });
 
 module.exports = nextConfig;
