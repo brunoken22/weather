d706a240a42a2aa59cfe3da8d6a2b4ab39633394
brunoken22 <bruno_am_22@hotmail.com>
2023-08-09 16:27:15 -0300

    FUNCIONANDO

--- app/mapa/page.tsx
+++ app/mapa/page.tsx
@@ -1,7 +1,41 @@
+'use client';
+import mapboxgl from 'mapbox-gl';
+mapboxgl.accessToken =
+  'pk.eyJ1IjoiZGVuaXNwYXJhZGEiLCJhIjoiY2t2cmhwbjZlMDM5czJ2cWlyczZoODg4cSJ9.6obRc3i_TK7qdx_A6_y-qg';
+import 'mapbox-gl/dist/mapbox-gl.css';
+import {useEffect, useRef, useState} from 'react';
+import {DivMap, Sidebar} from '@/ui/contenedores';
 export default function Mapa() {
+  const mapContainer = useRef(null);
+  const map: any = useRef(null);
+  const [lng, setLng] = useState(-70.9);
+  const [lat, setLat] = useState(42.35);
+  const [zoom, setZoom] = useState(9);
+  useEffect(() => {
+    if (map?.current) return; // initialize map only once
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current as any,
+      style: 'mapbox://styles/mapbox/streets-v12',
+      center: [lng, lat],
+      zoom: zoom,
+    });
+    map.current?.on('move', () => {
+      setLng(map.current.getCenter().lng.toFixed(4));
+      setLat(map.current.getCenter().lat.toFixed(4));
+      setZoom(map.current.getZoom().toFixed(2));
+    });
+  }, [lng, lat, zoom]);
+
   return (
-    <>
-      <h1></h1>
-    </>
+    <DivMap>
+      <Sidebar>
+        Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+      </Sidebar>
+      <div
+        ref={mapContainer}
+        className='map-container'
+        style={{height: '100%'}}
+      />
+    </DivMap>
   );
 }
